0b4a8282af5c7500581295c05270572814671615
Seth Miller <millerado@gmail.com>
2023-04-04 09:38:13 -0600

    set up routes

---
+++ src/compenets/Main.js
@@ -0,0 +1,14 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export default function Main() {
+  return (
+    <div className='main-content'>
+      <Routes>
+        <Route 
+        path='/'
+        element={}
+        />
+      </Routes>
+    </div>
+  );
+}
