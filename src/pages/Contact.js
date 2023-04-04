df3c81e5ee9350183fef73f51476d746e0c2597e
Seth Miller <millerado@gmail.com>
2023-04-04 10:14:27 -0600

    add basic info

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>RCG Board COntact Information</h2>
+      <p>Bryan Miller cbryanmiller@yahoo.com</p>
+      <h2>Location</h2>
+      <p>4265 Ranch Drive, next to Union just south of Academy Blvd.</p>
+    </div>
+  );
+}
