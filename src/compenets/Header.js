7fea298351d310222ea7cfdbb2104678f5ae79ad
Seth Miller <millerado@gmail.com>
2023-04-04 09:38:52 -0600

    create basic componenets

---
+++ src/compenets/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h2>Ranch Community Garden</h2>
+      <nav>
+        <div>Home</div>
+        <div>Garden Information</div>
+        <div>Contact</div>
+      </nav>
+    </header>
+  );
+}
