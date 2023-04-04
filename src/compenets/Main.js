c073715aec3603ae4b6f9c6325dcc7583057a90b
Seth Miller <millerado@gmail.com>
2023-04-04 10:15:25 -0600

    add basic routes

--- src/compenets/Main.js
+++ src/compenets/Main.js
@@ -1,12 +1,16 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
+import Info from '../pages/Info';
+import Contact from '../pages/Contact';
 
 export default function Main() {
   return (
     <div className='main-content'>
       <h1>Main Content</h1>
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/info' element={<Info />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </div>
   );
