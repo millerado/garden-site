5a1a0ce6fb43b445c9778b8fa237e6ac01284f4b
Seth Miller <millerado@gmail.com>
2023-04-04 10:43:09 -0600

    add links to nav

--- src/compenets/Header.js
+++ src/compenets/Header.js
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header>
       <h2>Ranch Community Garden</h2>
       <nav>
-        <div>Home</div>
-        <div>Garden Information</div>
-        <div>Contact</div>
+        <Link to='/'>
+          <div>Home</div>
+        </Link>
+        <Link to='/info'>
+          <div>Garden Information</div>
+        </Link>
+        <Link to='/contact'>
+          <div>Contact</div>
+        </Link>
       </nav>
     </header>
   );
