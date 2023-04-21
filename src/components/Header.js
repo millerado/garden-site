8182abd49a43293b8216ad3697741c2e6d39872f
Seth Miller <millerado@gmail.com>
2023-04-21 09:56:31 -0600

    add form link

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,18 @@ export default function Header() {
         <Link to='/contact'>
           <div>Contact</div>
         </Link>
+        <Link
+          to='https://docs.google.com/forms/d/e/1FAIpQLSe08KfiU-qEYFEOcOGO6WfUzLOzLQNLyTrZZoOKeyBE56OOqQ/viewform?embedded=true&formkey=dFZYV2d2MWVrbUZpWW1ZaVUzWWxGTHc6MQ'
+          target='_blank'
+        >
+          <div>New Plot Application</div>
+        </Link>
+        <Link
+          to='https://docs.google.com/forms/d/e/1FAIpQLSe08KfiU-qEYFEOcOGO6WfUzLOzLQNLyTrZZoOKeyBE56OOqQ/viewform?embedded=true&formkey=dFZYV2d2MWVrbUZpWW1ZaVUzWWxGTHc6MQ'
+          target='_blank'
+        >
+          <div>Returning Plot Reneweal</div>
+        </Link>
       </nav>
     </header>
   );
