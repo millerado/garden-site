e65252cc117efc2be96fcd7f8432d2db681c46c0
Seth Miller <millerado@gmail.com>
2023-04-28 08:37:54 -0600

    add contact info and picture

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,5 @@
+import map from '../images/garden-location.png';
+
 export default function Contact() {
   return (
     <div>
@@ -7,8 +9,12 @@ export default function Contact() {
       <div className='text'>
         <h2>RCG Board Contact Information</h2>
         <p>Bryan Miller cbryanmiller@yahoo.com</p>
+        <p>Jeanette Martin jnette@jmseating.com</p>
+        <p>Shane Stutzman coloradogardens@gmail.com</p>
+        <p>Terry Webb tjhw63@gmail.com</p>
         <h2>Location</h2>
         <p>4265 Ranch Drive, next to Union just south of Academy Blvd.</p>
+        <img src={map} alt='Garden Location' className='loc-pic'></img>
       </div>
     </div>
   );
