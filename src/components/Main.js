d5e60f980076f995cf7bc91180099d090f7f6ef6
Seth Miller <millerado@gmail.com>
2023-04-28 08:38:18 -0600

    removed unused stuff

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,12 @@ import Home from '../pages/Home';
 import Info from '../pages/Info';
 import Contact from '../pages/Contact';
 import image1 from '../images/IMG_0842.JPG';
-import image2 from '../images/maple_hill.jpg';
 
 export default function Main() {
   return (
     <div className='main-content'>
       <Routes>
-        <Route path='/' element={<Home image={image1} />} />
+        <Route path='/' element={<Home />} />
         <Route path='/info' element={<Info />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
