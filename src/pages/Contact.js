3f6658faa9de5745079b33975a66fdd79ecc14cc
Seth Miller <millerado@gmail.com>
2023-04-28 08:49:33 -0600

    updated contact info

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,9 +9,10 @@ export default function Contact() {
       <div className='text'>
         <h2>RCG Board Contact Information</h2>
         <p>Bryan Miller cbryanmiller@yahoo.com</p>
+        <p>Terry Webb tjhw63@gmail.com</p>
         <p>Jeanette Martin jnette@jmseating.com</p>
         <p>Shane Stutzman coloradogardens@gmail.com</p>
-        <p>Terry Webb tjhw63@gmail.com</p>
+        <p>Cynthia Wisekal cynthia.wisekal@gmail.com</p>
         <h2>Location</h2>
         <p>4265 Ranch Drive, next to Union just south of Academy Blvd.</p>
         <img src={map} alt='Garden Location' className='loc-pic'></img>
